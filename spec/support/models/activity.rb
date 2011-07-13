fb1da834e8e415d96450cd91ce56416ba45753cf
Joseph Chen <joe1chen@hotmail.com>
2011-07-12 18:16:32 -0700

    Fix the activity class used in rspec tests to cache :comment field. Also need to add :receiver into the activity now that we have modified schema for sharding.

--- spec/support/models/activity.rb
+++ spec/support/models/activity.rb
@@ -3,8 +3,9 @@ class Activity
   
   activity :enquiry do
     actor :user, :cache => [:full_name]
-    object :enquiry, :cache => [:subject]
+    object :enquiry, :cache => [:comment]
     target :listing, :cache => [:title]
+    receiver :user, :cache => []
   end
   
 end
