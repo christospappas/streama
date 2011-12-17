e47acc5bc08c53fc61d9d7c083cbfef2a5d09b1a
Christos Pappas <christos.pappas@gmail.com>
2011-12-17 17:25:37 +1000

    clean up spec_helper.rb and move test models to spec/app/models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,45 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
-require 'rspec'
+MODELS = File.join(File.dirname(__FILE__), "app/models")
+SUPPORT = File.join(File.dirname(__FILE__), "support")
+$LOAD_PATH.unshift(MODELS)
+$LOAD_PATH.unshift(SUPPORT)
+
 require 'streama'
 require 'mongoid'
+require 'rspec'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+LOGGER = Logger.new($stdout)
+DATABASE_ID = Process.pid
 
 Mongoid.configure do |config|
-  name = "streama-rspec-test"
-  host = "localhost"
-  config.master = Mongo::Connection.new.db(name)
+  database = Mongo::Connection.new.db("mongoid_#{DATABASE_ID}")
+  database.add_user("mongoid", "test")
+  config.master = database
+  config.logger = nil
+end
+
+Dir[ File.join(MODELS, "*.rb") ].sort.each do |file|
+  name = File.basename(file, ".rb")
+  autoload name.camelize.to_sym, name
+end
+
+Dir[ File.join(SUPPORT, "*.rb") ].each do |file|
+  require File.basename(file)
 end
 
 RSpec.configure do |config|
   config.include RSpec::Matchers
   config.include Mongoid::Matchers
   config.mock_with :rspec
-  config.after :each do
-    Mongoid.master.collections.select { |c| c.name != 'system.indexes' }.each(&:drop)
+  
+  config.before(:each) do
+    Mongoid::IdentityMap.clear
   end
+  
+  config.after :suite do
+    Mongoid.master.connection.drop_database("mongoid_#{DATABASE_ID}")
+  end
+  
 end
