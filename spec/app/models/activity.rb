c9a3c3e891bcb58b2d20b96b1c5b0ee7c69986b6
Christos Pappas <christos.pappas@gmail.com>
2012-01-03 16:48:50 +1000

    clean up specs and test activities that don't cache fields

--- spec/app/models/activity.rb
+++ spec/app/models/activity.rb
@@ -1,10 +1,21 @@
 class Activity
   include Streama::Activity
   
-  activity :enquiry do
+  activity :new_enquiry do
     actor :user, :cache => [:full_name]
-    object :enquiry, :cache => [:subject]
+    object :enquiry, :cache => [:comment]
     target :listing, :cache => [:title]
   end
   
+  activity :new_enquiry_without_cache do
+    actor :user
+    object :enquiry
+    target :listing
+  end
+  
+  activity :new_comment do
+    actor :user, :cache => [:full_name]
+    object :listing
+  end
+    
 end
--- spec/lib/activity_spec.rb
+++ spec/lib/activity_spec.rb
@@ -6,28 +6,48 @@ describe "Activity" do
   let(:listing) { Listing.create(:title => "A test listing") }
   let(:user) { User.create(:full_name => "Christos") }
 
-  describe '.activity' do
+  describe ".activity" do
     it "registers and return a valid definition" do
-      @definition = Activity.activity(:new_enquiry) do
+      @definition = Activity.activity(:test_activity) do
         actor :user, :cache => [:full_name]
-        object :enquiry, :cache => [:comment]
         object :listing, :cache => [:title, :full_address]
         target :listing, :cache => [:title]
       end
       
       @definition.is_a?(Streama::Definition).should be true
     end
+    
   end
   
-  describe '#publish' do
-    
-    it "overrides the recievers if option passed" do
-      send_to = []
-      2.times { |n| send_to << User.create(:full_name => "Custom Receiver #{n}") }
+  describe "#publish" do
+
+    before :each do
+      @send_to = []
+      2.times { |n| @send_to << User.create(:full_name => "Custom Receiver #{n}") }
       5.times { |n| User.create(:full_name => "Receiver #{n}") }
-      @activity = Activity.publish(:new_enquiry, {:actor => user, :object => enquiry, :target => listing, :receivers => send_to})
+    end
+    
+    it "pushes activity to receivers" do
+      @activity = Activity.publish(:new_enquiry, {:actor => user, :object => enquiry, :target => listing, :receivers => @send_to})
       @activity.receivers.size.should == 2
     end
+
+
+    context "when activity not cached" do
+      
+      it "pushes activity to receivers" do
+        @activity = Activity.publish(:new_enquiry_without_cache, {:actor => user, :object => enquiry, :target => listing, :receivers => @send_to})
+        @activity.receivers.size.should == 2
+      end
+      
+    end
+    
+    it "overrides the recievers if option passed" do
+      @activity = Activity.publish(:new_enquiry, {:actor => user, :object => enquiry, :target => listing, :receivers => @send_to})
+      @activity.receivers.size.should == 2
+    end
+    
+
     
     context "when republishing"
       before :each do
@@ -44,14 +64,14 @@ describe "Activity" do
       end
   end
   
-  describe '.publish' do
+  describe ".publish" do
     it "creates a new activity" do
       activity = Activity.publish(:new_enquiry, {:actor => user, :object => enquiry, :target => listing})
       activity.should be_an_instance_of Activity
     end
   end
 
-  describe '#refresh' do
+  describe "#refresh" do
     
     before :each do
       @user = user
@@ -70,7 +90,7 @@ describe "Activity" do
     
   end
 
-  describe '#load_instance' do
+  describe "#load_instance" do
     
     before :each do
       @activity = Activity.publish(:new_enquiry, {:actor => user, :object => enquiry, :target => listing})
